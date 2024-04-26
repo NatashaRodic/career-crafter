ed934ed11ce82cc8a80b134850ecfa42cd6d8ea8
Natasha Rodic <natashawolt@gmail.com>
2024-04-26 18:02:20 -0400

    Create path for applications page

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('applications/', views.applications_index, name='index')
 ]
