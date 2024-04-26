fee6f67605c70ef0f43e4a51516ca9db53adaf6a
Natasha Rodic <natashawolt@gmail.com>
2024-04-26 18:15:29 -0400

    Create about page

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('applications/', views.applications_index, name='index')
+    path('about/', views.about, name='about')
 ]
