b5099efdc9bd7f71c2580b2c28d24bdf94bca64f
Natasha Rodic <natashawolt@gmail.com>
2024-04-27 10:48:29 -0400

    Change url from 'index' to applicatons_index

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('index/', views.index, name='index')
+    path('applications/', views.applications_index, name='index')
 ]
