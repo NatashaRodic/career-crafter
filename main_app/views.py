3f35bad0ff001c647c26b19e37a4107a7b7a3f58
Natasha Rodic <natashawolt@gmail.com>
2024-04-27 12:19:20 -0400

    Update applications/index.html file

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
 
+# Temporar Model for Applications
+applications = [
+    {
+        "job_title": "Front-end Developer",
+        "description": "Develop user-facing features using React.js and other front-end technologies",
+        "id": 1,
+        "location": "San Francisco, CA",
+        "dateApplied": "2024-04-25",
+        "datePosted": "2024-04-20",
+        "coverLetter": True,
+        "status": "Pending"
+    },
+    {
+        "job_title": "Data Scientist",
+        "description": "Analyze large datasets to provide insights and develop machine learning models",
+        "id": 2,
+        "location": "New York, NY",
+        "dateApplied": "2024-04-24",
+        "datePosted": "2024-04-15",
+        "coverLetter": True,
+        "status": "Under Review"}
+]
+
 # Create your views here.
 
 def home (request):
