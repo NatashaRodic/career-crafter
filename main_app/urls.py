0fd2bf9ce576085d794df8cbe86573a0b434f8cf
Natasha Rodic <natashawolt@gmail.com>
2024-04-28 23:20:37 -0400

    Add a route for application details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('applications/', views.applications_index, name='index')
+    path('applications/', views.applications_index, name='index'),
+    path('applications/<int:application_id>/', views.applications_detail, name='detail')
 ]
