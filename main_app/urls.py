3d1671454099603d8f7294b2ec7260aeb69c2df4
Natasha Rodic <natashawolt@gmail.com>
2024-04-29 01:31:02 -0400

    Add route for create application funcionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('applications/', views.applications_index, name='index'),
-    path('applications/<int:application_id>/', views.applications_detail, name='detail')
+    path('applications/<int:application_id>/', views.applications_detail, name='detail'),
+    path('applications/create/', views.ApplicationCreate.as_view(), name='application_create')
 ]
