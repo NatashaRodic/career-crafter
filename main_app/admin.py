f9d8fa93110a00c71da2e1a7e06f241ea19b31a2
Natasha Rodic <natashawolt@gmail.com>
2024-04-29 17:51:26 -0400

    Add action to the admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Application
+from .models import Application, Action
 
 # Register your models here.
-admin.site.register(Application)
+admin.site.register([Application, Action])
