c73df3bae38c859187c7c1220659bf45051b068d
Natasha Rodic <natashawolt@gmail.com>
2024-04-28 20:34:20 -0400

    Import Application model from Neon db

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,6 @@
 from django.shortcuts import render
+from .models import Application
 
-# Temporar Model for Applications
-applications = [
-    {
-        "job_title": "Front-end Developer",
-        "link": "",
-        "id": 1,
-        "location": "San Francisco, CA",
-        "dateApplied": "2024-04-25",
-        "datePosted": "2024-04-20",
-        "coverLetter": True,
-        "status": "Pending"
-    },
-    {
-        "job_title": "Data Scientist",
-        "link": "",
-        "id": 2,
-        "location": "New York, NY",
-        "dateApplied": "2024-04-24",
-        "datePosted": "2024-04-15",
-        "coverLetter": True,
-        "status": "Under Review"}
-]
 
 # Create your views here.
 
@@ -32,6 +11,7 @@ def about (request):
     return render(request, 'about.html')
 
 def applications_index (request):
+    applications = Application.objects.all()
     return render(request, 'applications/index.html', {
         'applications': applications
     })
