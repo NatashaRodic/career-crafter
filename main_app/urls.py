39bbe1c440409c026992ac261816c9ab0b976e0c
Natasha Rodic <natashawolt@gmail.com>
2024-04-30 19:07:29 -0400

    Create the signu up new user

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('applications/<int:pk>/update', views.ApplicationUpdate.as_view(), name='application_update'),
     path('applications/<int:pk>/delete', views.ApplicationDelete.as_view(), name='application_delete'),
     path('applications/<int:application_id>/add_action/', views.add_action, name='add_action'),
-    path('applications/<int:application_id>/add_note/', views.add_note, name='add_note')
+    path('applications/<int:application_id>/add_note/', views.add_note, name='add_note'),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import Application
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .forms import ActionForm, ApplicationForm, NoteForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -59,3 +61,18 @@ class ApplicationUpdate(UpdateView):
 class ApplicationDelete(DeleteView):
     model = Application
     success_url = '/applications/'
+
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
