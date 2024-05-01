6666bd42eee454959a54d0b8b639177f336bef50
Natasha Rodic <natashawolt@gmail.com>
2024-04-30 22:54:30 -0400

    Create Skill model

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,13 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+     
+    def get_absolute_url(self):
+        return reverse('skill_detail', kwargs={'pk': self.id})
 
 class Application(models.Model):
     STATUS = (
@@ -21,6 +28,8 @@ class Application(models.Model):
     cover_letter_included = models.BooleanField(default=False)
     status = models.CharField(max_length=2, choices=STATUS, default='AP')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    skills = models.ManyToManyField(Skill)
+
 
     def __str__(self):
         return f'{self.get_status_display()} '
@@ -40,6 +49,8 @@ class Action(models.Model):
 class Note(models.Model):
     note = models.TextField(max_length=300)
     application = models.ForeignKey(Application, on_delete=models.CASCADE)
+
+
 
 
 
