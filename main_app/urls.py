bfd0f9b04a9fc02fa1f2733e0436d229f4bf71e9
Natasha Rodic <natashawolt@gmail.com>
2024-05-02 12:09:21 -0400

    Add paths for skills and tags

--- main_app/urls.py
+++ main_app/urls.py
@@ -12,4 +12,11 @@ urlpatterns = [
     path('applications/<int:application_id>/add_action/', views.add_action, name='add_action'),
     path('applications/<int:application_id>/add_note/', views.add_note, name='add_note'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('skills/', views.SkillList.as_view(), name='skills_index'),
+    path('skills/<int:pk>/', views.SkillDetail.as_view(), name='skills_detail'),
+    path('skills/create', views.SkillCreate.as_view(), name='skills_create'),
+    path('applications/<int:application_id>/assoc_skill/<int:skill_id>/', views.assoc_skill, name='assoc_skill'),
+    path('tags/create', views.TagCreate.as_view(), name='tags_create'),
+    path('applications/<int:application_id>/assoc_tag/<int:tag_id>/', views.assoc_tag, name='assoc_tag'),
+
 ]
